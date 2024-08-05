b64196078cee05223a0d48203cf2f32ee0b5e9c9
BryanEspR <93746466+BryanEspR@users.noreply.github.com>
2024-08-05 17:08:43 -0600

    Create BryanEspR.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/BryanEspR.py
@@ -0,0 +1,30 @@
+# https://www.python.org/
+
+# Comentario en una línea en Python
+
+"""
+Los comentarios en varias líneas en Python se declaran abriendo con tres dobles comillas
+y cerrando entre tres dobles comillas. Lo que sea que se escriba entre estos símbolos
+Python lo considerará un comentario en varias líneas
+"""
+
+'''
+Lo mismo aplica con comillas sencillas,
+también es posible hacer comentarios
+en múltiples líneas
+'''
+
+# Creando una variable en Python
+age = 27
+
+# Creando variables de los distintos tipos que hay en Python
+variable_string = "Bryan"
+
+variable_integer = 27
+
+variable_float = 27.5
+
+variable_boolean = True
+
+# Imprimir en el terminal el texto "Hola" + Leguaje de Programación
+print("Hola Python")
